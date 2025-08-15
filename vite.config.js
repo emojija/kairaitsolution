0d5fc4d9308e71eb024234c8870b5d64c850dd15
Ankit Bisht <ankitbisht9837@gmail.com>
2025-08-15 22:56:10 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,23 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import fs from "fs";
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/', // Use absolute paths for production
-  plugins: [react(), tailwindcss()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false
-  }
-})
+  server: {
+    port: 5173,
+  },
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  css: {
+    modules: false,
+  },
+});
